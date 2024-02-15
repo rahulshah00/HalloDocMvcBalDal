83cd56d63305e12115def4ee6dceaf2ea4686773
rahulshah00 <rahul.shah@etatvasoft.com>
2024-02-15 18:35:00 +0530

    Review

---
+++ DAL/ViewModels/PatientModel.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace DAL.ViewModels
+{
+    public class PatientModel
+    {
+        public string Symptoms { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string DateOfBirth { get; set; }
+        public string Email { get; set; }
+        public string PhoneNo { get; set; }
+        public string Street  { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+        public string RoomSuite { get; set; }
+        public IFormFile File{ get; set; } 
+        public string Password { get; set; }
+        public string ConfirmPass{ get; set; }
+        
+    }
+}
